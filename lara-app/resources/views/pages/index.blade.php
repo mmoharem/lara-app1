edfa94dac160f798a16d419ebd514895eb4a3127
mmoharem <cloud@bnfstore.com>
2018-04-20 15:02:32 +0200

    after database migration

--- lara-app/resources/views/pages/index.blade.php
+++ lara-app/resources/views/pages/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="jumbotron text-center">
     <h1>{{$title}}</h1>
-    <p>this welcom page.</p>
+    <p>this welcom page.</p> 
+    <p><a href="/login" class="btn btn-primary btn-lg">Login</a> <a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p> 
+  </div>
+    
 @endsection
 
